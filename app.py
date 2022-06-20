bc3fd1282acff6a77b31d8073481eee045c06378
Muhamad Taopik <mr.94t3z@gmail.com>
2022-06-20 17:10:52 +0700

    🐍

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 import secrets
+import time
 from cmath import e
 from flask import Flask, flash, request, redirect, render_template, url_for, current_app
 from flask_sqlalchemy import SQLAlchemy
@@ -7,7 +8,6 @@ from flask_login import UserMixin, login_user, LoginManager, login_required, log
 from flask_uploads import IMAGES, UploadSet, configure_uploads
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_migrate import Migrate
-from datetime import datetime
 from sqlalchemy.orm import relationship, backref
 
 # Add Database
@@ -93,7 +93,7 @@ class Peminjamans(db.Model):
     id_user = db.Column(db.Integer, db.ForeignKey(
         'users.id'),  nullable=False)
     tgl_pinjam = db.Column(
-        db.DateTime(), default=datetime.today().replace(microsecond=0))
+        db.DateTime(), default=time.strftime("%Y-%m-%d %H:%M:%S"))
     qty = db.Column(db.Integer, nullable=False)
     status = db.Column(db.Boolean, default=False)
 
@@ -117,7 +117,7 @@ class Pengembalians(db.Model):
     id_peminjaman = db.Column(
         db.Integer, db.ForeignKey('peminjamans.id_peminjaman'), nullable=False)
     tgl_pengembalian = db.Column(
-        db.DateTime(), default=datetime.today().replace(microsecond=0))
+        db.DateTime(), default=time.strftime("%Y-%m-%d %H:%M:%S"))
 
     barang = db.relationship(Barangs, backref=backref(
         "pengembalians", cascade="all, delete-orphan"))
