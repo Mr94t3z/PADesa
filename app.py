e57083a6de2d77da533217cfc6bf594c005c7344
Muhamad Taopik <mr.94t3z@gmail.com>
2022-06-20 16:52:14 +0700

    🐍

--- app.py
+++ app.py
@@ -93,7 +93,7 @@ class Peminjamans(db.Model):
     id_user = db.Column(db.Integer, db.ForeignKey(
         'users.id'),  nullable=False)
     tgl_pinjam = db.Column(
-        db.DateTime(), default=date.today())
+        db.Date(), default=date.today())
     qty = db.Column(db.Integer, nullable=False)
     status = db.Column(db.Boolean, default=False)
 
@@ -117,7 +117,7 @@ class Pengembalians(db.Model):
     id_peminjaman = db.Column(
         db.Integer, db.ForeignKey('peminjamans.id_peminjaman'), nullable=False)
     tgl_pengembalian = db.Column(
-        db.DateTime(), default=date.today())
+        db.Date(), default=date.today())
 
     barang = db.relationship(Barangs, backref=backref(
         "pengembalians", cascade="all, delete-orphan"))
